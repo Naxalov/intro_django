fc98f03227cf9dc0a3093110d82356072dd53366
ZARIFMAC <uzbzarif@gmail.com>
2024-02-06 15:53:59 +0500

    json

--- intro/urls.py
+++ intro/urls.py
@@ -1,11 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpRequest,JsonResponse
 from django.urls import path
 
-def index(request):
-    print('Hello, World!')
-    return HttpResponse('Hello, World!')
-    
+def index(request: HttpRequest):
+    # Get query parameters
+    a = request.GET.get('a', '0')   # default value is '0'
+    b = request.GET.get('b', '0')   # default value is '0'
+    data = {
+        'a': a,
+        'b': b
+    }
+    return JsonResponse(data)
+
+ 
 urlpatterns = [
     
-    path('',index)
+    path('',index),
+
 ]
